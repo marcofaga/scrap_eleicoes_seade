f81fd29bb0c9925c458c8655ffcf0f3fce3944ef
marcofaga <marcofaga@gmail.com>
2018-02-08 16:04:57 -0200

    Versão 1.0

--- crawler_seade/crawler_seade/spiders/spider_crawler_seade.py
+++ crawler_seade/crawler_seade/spiders/spider_crawler_seade.py
@@ -1,32 +1,116 @@
 import scrapy
-import datetime
+import re
+import csv
+import unidecode
+import time
+# import datetime
 
-anos = ['1982', '1988', '1992']
+ano = ['1982', '1988', '1992']
+
 
 class crawler_seade(scrapy.Spider):
     name = "crawler_seade"
-    login_url = 'http://produtos.seade.gov.br/produtos/moveleitoral/index.php?tip=5'
-    start_urls = [login_url]
-
+    login_url = 'http://produtos.seade.gov.br/produtos/moveleitoral/index.php'
+    login_url_du = 'http://produtos.seade.gov.br/produtos/moveleitoral/index.php?car=4'
+    inicio = 0
+    inicio_ano = 0
+    start_urls = [
+        'http://produtos.seade.gov.br/produtos/moveleitoral/index.php?tip=5'
+    ]
+    listacity = []
+    listacityval = []
 
     def parse(self, response):
-        yield scrapy.FormRequest(url='http://produtos.seade.gov.br/produtos/moveleitoral/index.php',
-            formdata={'loc': '4', 'ano_list[]': '1988'},
-            callback=self.cidade
+        year = ano[self.inicio_ano]
+        request = scrapy.FormRequest(
+            url=self.login_url,
+            formdata={'loc': '4', 'ano_list[]': year},
+            dont_filter=True,
+            callback=self.post_continue
         )
+        yield request
 
-    def cidade(self, response):
-        yield scrapy.FormRequest(url='http://produtos.seade.gov.br/produtos/moveleitoral/index.php',
+    def post_continue(self, response):
+        yield scrapy.FormRequest(
+            url=self.login_url,
             formdata={'ano': '4', 'l': 'M', 'mun': '', 'bot4': 'Continue'},
-            callback=self.cidades_scrap
+            dont_filter=True,
+            callback=self.post_cidade
         )
 
-    def cidades_scrap(self, response):
-        for each in response.css('option'):
-            yield {'cidade': each.css('::text').extract_first()}
+    def post_cidade(self, response):
+        if self.inicio == 0:
+            self.listacity.clear()
+            self.listacityval.clear()
+            cidade_txt = response.css('option::text').extract()
+            for city in cidade_txt:
+                self.listacity.append(unidecode.unidecode(re.sub("\n", "", str(city)).strip().upper()))
+            # self.listacity = self.listacity[:3]
 
-    def parse2(self, response):
-        print('EPALAIAP')
-        filename = 'quotes.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+            cidade_val = response.css('option::attr(value)').extract()
+            for city in cidade_val:
+                self.listacityval.append(city)
+            # self.listacityval = self.listacityval[:3]
+
+            x = self.inicio
+        else:
+            x = self.inicio
+
+        request = scrapy.FormRequest(
+            url=self.login_url_du,
+            formdata={'loca_list[]': self.listacityval[x]},
+            dont_filter=True,
+            callback=self.post_cargo
+        )
+        request.meta['cidade_txt'] = self.listacity[x]
+        yield request
+
+    def post_cargo(self, response):
+        request = scrapy.FormRequest(
+            url=self.login_url,
+            formdata={'res': '4', 'carg_list[]': '10', 'SAI': 'TELA'},
+            dont_filter=True,
+            callback=self.scrap
+        )
+        request.meta['cidade_txt'] = response.meta['cidade_txt']
+        yield request
+
+    def scrap(self, response):
+        dados = str(response.css('table').extract())
+        dados = re.sub("right", "left", dados)
+        lista = re.findall("<td align=\"left\">(.*?)</td>", dados)
+        x = len(lista)
+        listafin = []
+        while x != 0:
+            elementos = list(lista[:4])
+            elementos.append(response.meta['cidade_txt'])
+            elementos.append(ano[self.inicio_ano])
+            elementos[2] = int(re.sub('\.', '', elementos[2]))
+            listafin.append(elementos)
+            lista = lista[4:]
+            x = len(lista)
+
+        csvfile = 'dados_captados.csv'
+
+        with open(csvfile, 'a') as output:
+            writer = csv.writer(output, lineterminator='\n', delimiter=';', quotechar='"', quoting=csv.QUOTE_ALL)
+            writer.writerows(listafin)
+
+        request = scrapy.Request(url=self.start_urls[0], dont_filter=True, callback=self.parse)
+        if self.inicio == 0:
+            self.inicio += 1
+            yield request
+        else:
+            if self.inicio != len(self.listacityval) - 1:
+                self.inicio += 1
+                yield request
+            else:
+                if self.inicio_ano != len(ano) - 1:
+                    self.inicio_ano += 1
+                    print("Scrap do ano:",ano[self.inicio_ano])
+                    time.sleep(10)
+                    self.inicio = 0
+                    yield request
+                else:
+                    print("Dados Captados")
+                    return
---
+++ crawler_seade/crawler_seade/text.py
@@ -0,0 +1,2 @@
+lst = 'oie'
+print("Dados de",lst)
