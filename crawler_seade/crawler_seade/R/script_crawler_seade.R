633f114ac0e88aaa2e3af2f11d8b66f12e763f6a
Marco Antonio Faganello <marcofaga@gmail.com>
2018-03-07 22:49:03 -0300

    V1.3 - Inclusão dados de 1985

--- crawler_seade/crawler_seade/R/script_crawler_seade.R
+++ crawler_seade/crawler_seade/R/script_crawler_seade.R
@@ -67,3 +67,33 @@ write.csv(bd01_pref_sp,"../arquivos_finais/dados_captados_prefeito.csv",row.name
 write.csv(bd02_ver_sp,"../arquivos_finais/dados_captados_vereador.csv",row.names = F)
 remove(apag, apag2, apag3, apag4, apag5, each)
 save.image("crawler_seade_R.RData")
+
+#07 de março de 2018
+#Adicionando dados de 1985 à base
+remove(list = ls())
+load("crawler_seade_R.RData")
+apag <- read.csv("../arquivos_finais/dados_captados_1985.csv",sep=";",header = F)
+names(apag) <- c("candidato","partido","votosnom","votosper","municipio","eleicao","cargo")
+apag$turno <- "1"
+apag$key <- paste(apag$municipio, apag$eleicao,sep="_")
+apag$key2 <- c(1:dim(apag)[1])
+apag2 <- unique(apag$key)
+apag$situacao <- "NAO ELEITO"
+for(each in apag2) {
+  
+  apag3 <- apag[apag$key == each,]
+  apag3 <- apag3$key2[apag3$votosnom == max(apag3$votosnom)]
+  apag$situacao[apag$key2 == apag3] <- "ELEITO" 
+  
+  
+}
+apag <- apag[,-c(9,10)]
+apag2 <- read.csv("../arquivos_finais/dados_captados_prefeito.csv", sep=",", header = T)
+apag3 <- apag2[apag2$eleicao >= 1988,]
+apag2 <- apag2[apag2$eleicao < 1988,]
+apag4 <- rbind(apag2, apag, apag3)
+bd01_pref_sp <- apag4
+remove(apag, apag2, apag3, apag4, each)
+write.csv(bd01_pref_sp,"../arquivos_finais/dados_captados_prefeito.csv",row.names = F)
+index <- insereindex("bd01_pref_sp","Dados das Eleições Municipais de 1982, 1985, 1988 e 1992 de todo o Estado de SP")
+save.image("crawler_seade_R.RData")
---
+++ crawler_seade/crawler_seade/spiders/spider_crawler_seade_1985.py
@@ -0,0 +1,138 @@
+import scrapy
+import re
+import csv
+import unidecode
+import time
+# import datetime
+
+ano = ['1985']
+
+
+def unique(sequence):
+    seen = set()
+    return [x for x in sequence if not (x in seen or seen.add(x))]
+
+
+class crawler_seade(scrapy.Spider):
+    name = "crawler_seade_1985"
+    login_url = 'http://produtos.seade.gov.br/produtos/moveleitoral/index.php'
+    login_url_du = 'http://produtos.seade.gov.br/produtos/moveleitoral/index.php?car=4'
+    inicio = 0
+    inicio_ano = 0
+    start_urls = [
+        'http://produtos.seade.gov.br/produtos/moveleitoral/index.php?tip=5'
+    ]
+    listacity = []
+    listacityval = []
+
+    def parse(self, response):
+        year = ano[self.inicio_ano]
+        request = scrapy.FormRequest(
+            url=self.login_url,
+            formdata={'loc': '4', 'ano_list[]': year},
+            dont_filter=True,
+            callback=self.post_continue
+        )
+        yield request
+
+    def post_continue(self, response):
+        yield scrapy.FormRequest(
+            url=self.login_url,
+            formdata={'ano': '4', 'l': 'M', 'mun': '', 'bot4': 'Continue'},
+            dont_filter=True,
+            callback=self.post_cidade
+        )
+
+    def post_cidade(self, response):
+        if self.inicio == 0:
+            self.listacity.clear()
+            self.listacityval.clear()
+            cidade_txt = response.css('option::text').extract()
+            for city in cidade_txt:
+                self.listacity.append(
+                    unidecode.unidecode(
+                        re.sub("\n", "", str(city)).strip().upper()
+                    )
+                )
+            # self.listacity = self.listacity[:3]
+
+            cidade_val = response.css('option::attr(value)').extract()
+            for city in cidade_val:
+                self.listacityval.append(city)
+            # self.listacityval = self.listacityval[:3]
+
+            self.listacity = unique(self.listacity)
+            self.listacityval = unique(self.listacityval)
+
+            x = self.inicio
+        else:
+            x = self.inicio
+
+        request = scrapy.FormRequest(
+            url=self.login_url_du,
+            formdata={'loca_list[]': self.listacityval[x]},
+            dont_filter=True,
+            callback=self.post_cargo
+        )
+        request.meta['cidade_txt'] = self.listacity[x]
+        yield request
+
+    def post_cargo(self, response):
+        request = scrapy.FormRequest(
+            url=self.login_url,
+            formdata={'res': '4', 'carg_list[]': '10', 'SAI': 'TELA'},
+            dont_filter=True,
+            callback=self.scrap
+        )
+        request.meta['cidade_txt'] = response.meta['cidade_txt']
+        yield request
+
+    def scrap(self, response):
+        dados = str(response.css('table').extract())
+        dados = re.sub("right", "left", dados)
+        lista = re.findall("<td align=\"left\">(.*?)</td>", dados)
+        x = len(lista)
+        listafin = []
+        while x != 0:
+            elementos = list(lista[:4])
+            elementos.append(response.meta['cidade_txt'])
+            elementos.append(ano[self.inicio_ano])
+            elementos[2] = int(re.sub('\.', '', elementos[2]))
+            elementos.append("PREFEITO")
+            listafin.append(elementos)
+            lista = lista[4:]
+            x = len(lista)
+        listafin = [
+            listafin[x] for x in range(len(listafin))
+            if not(listafin[x] in listafin[:x])
+        ]
+
+        csvfile = 'dados_captados_1985.csv'
+
+        with open(csvfile, 'a') as output:
+            writer = csv.writer(
+                output, lineterminator='\n',
+                delimiter=';',
+                quotechar='"',
+                quoting=csv.QUOTE_ALL
+            )
+            writer.writerows(listafin)
+
+        request = scrapy.Request(url=self.start_urls[0], dont_filter=True, callback=self.parse)
+        if self.inicio == 0:
+            self.inicio += 1
+            yield request
+        else:
+            if self.inicio != len(self.listacityval) - 1:
+                self.inicio += 1
+                yield request
+            else:
+                if self.inicio_ano != len(ano) - 1:
+                    self.inicio_ano += 1
+                    print("Scrap do ano:",ano[self.inicio_ano])
+                    time.sleep(10)
+                    self.inicio = 0
+                    yield request
+                else:
+                    print("Dados Captados")
+                    return
